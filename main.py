419b392ec98e9397d4affe743fe58173f2ccaeeb
Tomek Wójcik <vooydzig@gmail.com>
2018-09-18 22:25:14 +0200

    added input source

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import csv_dict
 import csv_list
 
+# input data: https://datasource.kapsarc.org/explore/dataset/national-accounts-main-aggregates-database-1970-2014/table/?disjunctive.freq&disjunctive.location&disjunctive.variable
+
 GROUPPING = ['Year', 'FREQ']
 
 dict_data = csv_dict.read_csv_file('sample/big.csv', GROUPPING)
